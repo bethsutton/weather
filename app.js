759b6668381d741e46f60480a5e36d91f424463e
Beth Sutton <bksutton2@gmail.com>
2022-01-18 15:47:21 -0700

    added weather API fetch and get weather on page load

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-// Load API key from config file
-const myKey = config.key;
+// Init weather object
+const weather = new Weather('London', 'UK');
+
+// Get weather on DOM load
+
+document.addEventListener('DOMContentLoaded', getWeather);
+
+function getWeather() {
+  weather.getWeather()
+    .then(results => {
+      console.log(results);
+    })
+    .catch(err => console.log(err));
+}
---
+++ weather.js
@@ -0,0 +1,27 @@
+
+// Get API key from config file
+const myKey = Config.key;
+
+class Weather {
+  constructor(city, country) {
+    this.apikey = myKey;
+    this.city = city;
+    this.country = country;
+  }
+
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apikey}`);
+
+    const responseData = await response.json();
+
+    return responseData;
+  }
+
+  // Change weather location
+  changeLocation(city, country) {
+    this.city = city;
+    this.country = country;
+  }
+
+}
