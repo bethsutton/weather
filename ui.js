4ef410a863ef2d42acd2c02463bc6a469c1a2eeb
Beth Sutton <bksutton2@gmail.com>
2022-01-19 15:02:57 -0700

    added styling, changed data displayed in ui

--- ui.js
+++ ui.js
@@ -2,30 +2,32 @@ class UI {
   constructor() {
     this.location = document.getElementById('w-location');
     this.desc = document.getElementById('w-desc');
-    this.string = document.getElementById('w-string');
+    this.temp = document.getElementById('w-temp');
     this.details = document.getElementById('w-details');
     this.icon = document.getElementById('w-icon');
 
     // Main
     this.feelsLike = document.getElementById('w-feels-like');
     this.humidity = document.getElementById('w-humidity');
-    this.pressure = document.getElementById('w-pressure');
-    this.temp = document.getElementById('w-temp');
-    // this.tempMax = document.getElementById('w-tempMax');
-    // this.tempMin = document.getElementById('w-tempMin');
+    this.tempMax = document.getElementById('w-tempMax');
+    this.tempMin = document.getElementById('w-tempMin');
+    this.wind = document.getElementById('w-wind');
   }
 
   paint(weather) {
     this.location.textContent = `${weather.name}, ${weather.sys.country}`;
     this.desc.textContent = weather.weather[0].main;
-    this.string.textContent = `${weather.main.temp} °F`;
+    this.temp.textContent = `${Math.round(weather.main.temp)}°F`;
 
     const iconPng = weather.weather[0].icon;
     this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${iconPng}@2x.png`);
 
-    this.feelsLike.textContent = `Feels Like: ${weather.main.feels_like} °F`;
+    this.feelsLike.textContent = `Feels Like: ${Math.round(weather.main.feels_like)}°F`;
     this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}%`;
-    this.pressure.textContent = `Pressure: ${weather.main.pressure} hPa`;
-    this.temp.textContent = `Temperature: ${weather.main.temp} °F`;
+    this.tempMax.textContent = `H: ${Math.round(weather.main.temp_max)}°F`;
+    this.tempMin.textContent = `L: ${Math.round(weather.main.temp_min)}°F`;
+    this.wind.textContent = `Wind: ${weather.wind.speed} mph`;
+
+
   }
 }
